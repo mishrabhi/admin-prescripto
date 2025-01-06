2f800ff8d4039d33b2d0d483cefff75386f30643
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-07 01:27:48 +0530

    created add doctor form UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
+  const navigate = useNavigate();
 
   //Logout
   const logout = () => {
+    //navigate to home page
+    navigate("/");
+    //reset and remove token
     aToken && setAToken("");
     aToken && localStorage.removeItem("aToken");
   };
